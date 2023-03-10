65f799ff6d01852fa89b0be0b123484381f0639a
Louis <loui@tekswipe.com.au>
2023-03-10 18:23:19 +0800

    sidebar

--- comps/sidebar.js
+++ comps/sidebar.js
@@ -1,20 +1,42 @@
-import { sidebarData } from "./sidebarData";
 import styles from "./sidebar.module.css";
+import Sidebar from "./sidebar";
+import { useState } from "react";
+import { sidebarData } from "./sidebarData";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import Link from "next/link";
-const Sidebar = () => {
+const SidebarSlider = () => {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   return (
-    <div>
-      {sidebarData.map((item, index) => {
-        return (
-          <li key={index} className={styles.list1}>
-            <Link className={styles.list} href={item.path}>
-              {item.title}
+    <>
+      <div className={styles.navbar}>
+        <Link href="#" className={styles.menuBars}>
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
+      </div>
+      <nav className={sidebar ? styles.navMenuActive : styles.navMenu}>
+        <ul className={styles.navMenuItems} onClick={showSidebar}>
+          <li className={styles.navbarToggle}>
+            <Link href="#" className={styles.menuBars}>
+              <AiIcons.AiOutlineClose />
             </Link>
           </li>
-        );
-      })}
-    </div>
+          {sidebarData.map((item, index) => {
+            return (
+              <li key={index} className={styles.list1}>
+                <Link className={styles.list} href={item.path}>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
   );
 };
 
-export default Sidebar;
+export default SidebarSlider;
+
+//create a css style with overlay file for the sidebar component
